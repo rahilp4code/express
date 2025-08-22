4f04374d3783c98f2d53024156663072e265bd64
rahilp4code <pathanrahil2244@gmail.com>
2025-08-22 23:56:45 +0530

    sixth commit

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.use(express.json()); // this express.json here caliing this function basical
 
 //Creating middleware
 
-app.use((req, res, next) => {
-  console.log('Hello from the Middleware');
-  next(); // always remember to add next() or it will block the execution of middleware stack
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the Middleware');
+//   next(); // always remember to add next() or it will block the execution of middleware stack
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -35,6 +35,29 @@ if (process.env.NODE_ENV == 'development') {
 
 app.use('/api/v2/setups', setupRouter);
 app.use('/api/v2/users', userRouter);
-app.use(express.static(`${__dirname}/GamingGo/public`));
+// app.use(express.static(`${__dirname}/GamingGo/public`));
+
+// ROUTE FOR ANY URL THAT WE GET WHICH ISNT DEFINED
+
+app.use((req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this sever!..`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this sever!..`);
+  err.statusCode = 404;
+  err.status = 'fail';
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
